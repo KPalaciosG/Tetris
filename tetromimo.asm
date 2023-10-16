68b1b74fc6f30983a82ca531eb6726dafabea324
kattcon <89096036+kattcon@users.noreply.github.com>
2023-10-16 13:34:49 -0600

    Update tetromimo.asm
    bloques start

--- tetromimo.asm
+++ tetromimo.asm
@@ -1,1 +1,21 @@
+section .data
 
+matrix: resb 200 ; 20x10
+
+section .text
+
+;definicion de bloques
+
+;generacion de bloques
+
+;mover hacia la derecha
+
+;mover hacia la izquierda
+
+;rotar
+
+;verificar estado
+
+mov rax, 60
+mov rdi, 0
+syscall
