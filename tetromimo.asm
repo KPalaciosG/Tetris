2fb8c867555ae5a9b8a919fc58a690b26bd4c362
kattcon <89096036+kattcon@users.noreply.github.com>
2023-10-17 13:07:02 -0600

    Update tetromimo.asm
    se arregla código y se agrega block O

--- tetromimo.asm
+++ tetromimo.asm
@@ -1,28 +1,37 @@
+section .bss
+matrix: resb 200 ;se separan 200 espacios en memoria
+
 section .data
 
-x dd, 10
-y dd, 20
-matrix: resb 200 ; 20x10
 x: dd 10
 y: dd 20
 
 matrix: dd 200
 marixLen: equ $-matrix
+;matrix: dd 200
+marixLen: dd 200
 
 section .text
 global _start
 _start:
 
-;definicion de bloques
-;bloque I: [1][1][1][1]
 mov rbx, matrix 
 
 ;definicion de bloques
+;--------definicion de bloques---------
 ;bloque I: [1][1][1][1] color rojo
 mov byte[rbx+3], 1
 mov byte[rbx+4], 1
 mov byte[rbx+5], 1
 mov byte[rbx+6], 1
+
+;bloque O: [2][2] color azúl
+;          [2][2]
+mov byte[rbx+4], 2
+mov byte[rbx+5], 2
+mov byte[rbx+14], 2
+mov byte[rbx+15], 2
+
 
 ;generacion de bloques
 
