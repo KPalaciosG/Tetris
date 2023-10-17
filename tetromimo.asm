24f4adc19a00b72600fd573e6ed745e4f7a4a0a2
kattcon <89096036+kattcon@users.noreply.github.com>
2023-10-16 19:52:58 -0600

    Update tetromimo.asm

--- tetromimo.asm
+++ tetromimo.asm
@@ -1,21 +1,22 @@
 section .data
 
+x dd, 10
+y dd, 20
 matrix: resb 200 ; 20x10
+marixLen: equ $-matrix
 
 section .text
 
 ;definicion de bloques
+;bloque I: [1][1][1][1]
+
 
 ;generacion de bloques
 
 ;mover hacia la derecha
-
 ;mover hacia la izquierda
-
 ;rotar
-
 ;verificar estado
-
 mov rax, 60
 mov rdi, 0
 syscall
