c40ed0738874956c6c62d6abaf24fb54c52d14dc
kattcon <89096036+kattcon@users.noreply.github.com>
2023-10-17 20:44:20 -0600

    Update tetromimo.asm
    se agrega función moveRight que mueve los tetronimos hacia la derecha

--- tetromimo.asm
+++ tetromimo.asm
@@ -3,73 +3,51 @@ matrix: resb 200 ;se separan 200 espacios en memoria
 section .data
 x: dd 10
 y: dd 20
+
 marixLen: dd 200 ;tamaño de la matriz
+matrixLen: dd 200 ;tamaño de la matriz
+
 section .text
 global _start
-_start:
-mov rbx, matrix 
-;--------definicion de bloques---------
-;bloque I: [1][1][1][1] color rojo
-mov byte[rbx+3], 1
-mov byte[rbx+4], 1
-mov byte[rbx+5], 1
-mov byte[rbx+6], 1
-;bloque O: [2][2] color azúl
-;          [2][2]
-mov byte[rbx+4], 2
-mov byte[rbx+5], 2
-mov byte[rbx+14], 2
-mov byte[rbx+15], 2
-;bloque T:   [3]     color verde
-;         [3][3][3]
-mov byte[rbx+5], 3
-mov byte[rbx+14], 3
-mov byte[rbx+15], 3
-mov byte[rbx+16], 3
-;bloque S:   [4][4] color amarillo
-;         [4][4]
-mov byte[rbx+4], 4
-mov byte[rbx+5], 4
-mov byte[rbx+13], 4
-mov byte[rbx+14], 4
-;bloque Z:   [5][5]     color rosa
-;               [5][5]
-mov byte[rbx+4], 5
-mov byte[rbx+5], 5
-mov byte[rbx+15], 5
-mov byte[rbx+16], 5
-;bloque J: [6]         color morado
-;          [6][6][6]
-mov byte[rbx+4], 6
-mov byte[rbx+14], 6
-mov byte[rbx+15], 6
-mov byte[rbx+16], 6
-;bloque L        [7]  color naranja
-;          [7][7][7]
-mov byte[rbx+6], 7
-mov byte[rbx+14], 7
-mov byte[rbx+15], 7
-mov byte[rbx+16], 7
-;--------------end definicion de bloques----------
-
-;----------get block-----------
-getBlock:
-	mov rsi, [rbx]
-	cmp rsi, 0
-	mov sil, byte[rbx]
+@@ -70,14 +70,36 @@ getBlock:
 	cmp sil, 0
 	je increase
 
-	mov rax, [rsi]
-	jmp end ;CAMBIAR DESPUÉS CON EL SIGUIENTE CODIGO
 	mov rax, rsi
+	mov rax, rsi ;muevo a rax el tipo de bloque (1:I, 2:O, 3:T etc)
 	jmp end
 
 	increase: 
 	inc rbx
 	loop getBlock
 ;---------- end getBlock----------
+;---------- end getBlock-----------
+
+;-----------moveRight---------------
+;mover hacia la derecha
+;+1 para mover hacia la derecha, se empieza desde abajo de la matriz hacia arriba
+	
+moveRight:
+	
+	mov ecx, 200
+	mov rbx, matrix
+
+	startMoveRight:
+	cmp byte[rbx+rcx],0
+	je loopRight
+
+	mov dl, byte[rbx+rcx]
+	mov byte[rbx+rcx],0
+	mov byte[rbx+rcx+1], dl
+	
+	loopRight: 
+
+	loop startMoveRight	
+
+;-----------end moveRight-------------
+
 ;generacion de bloques
+
 ;mover hacia la derecha
 ;+1 para mover hacia la derecha
 ;mover hacia la izquierda
